69e714fa0abaca4858dd8f1d0dc2ad48484fa732
Gavin Gray <ggray5914@stu.neisd.net>
2024-03-26 10:04:25 -0500

    the idea

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+// invo System based off chatgpt suggestion cause idk what to do
+//add, remove, items in a arraylist of arraylists, maybe a search method using apcsa things, 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        //1
     }
 }
