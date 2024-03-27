5488f87894cc49774f5865ce978b6bc08d88cdfe
Gavin Gray <ggray5914@stu.neisd.net>
2024-03-27 10:44:23 -0500

    things

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@
 //add, remove, items in a arraylist of arraylists, maybe a search method using apcsa things, 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
         //1
     }
 }
---
+++ src/invontory.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+public class invontory {
+    ArrayList<HashMap> IVItems = new ArrayList<HashMap>();
+    HashMap <String,Integer> items  = new HashMap <String,Integer> ();
+    items.put("Bag",5);
+
+}
